767f449aaeb3a8c9b072e83e87307f1c62e0dcdc
Leo-Zubiri <leo.zubiri@outlook.com>
2024-01-01 21:52:48 -0600

    Add First Model (Address)

---
+++ Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreTestingMVC.Models
+{
+    public class Address
+    {
+        // Primary Key
+        [Key] public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
