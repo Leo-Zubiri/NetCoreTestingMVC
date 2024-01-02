842720a259394822427a8e2e43c9bfab68735778
Leo-Zubiri <leo.zubiri@outlook.com>
2024-01-01 22:04:34 -0600

    Add Model AppUser

---
+++ Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreTestingMVC.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int Mileage { get; set; }
+        public Address Address { get; set; }
+    }
+}
