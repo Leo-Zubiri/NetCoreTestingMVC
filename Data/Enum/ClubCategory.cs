85265b43415687cbf5280d2885cdf15ad1d8eac2
Leo-Zubiri <leo.zubiri@outlook.com>
2024-01-02 22:45:17 -0600

    Add Enum Club category

---
+++ Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreTestingMVC.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
--- Models/Club.cs
+++ Models/Club.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using NetCoreTestingMVC.Data.Enum;
 
 namespace NetCoreTestingMVC.Models
 {
@@ -14,5 +15,6 @@ namespace NetCoreTestingMVC.Models
         public Address? Address { get; set; }
         [ForeignKey("AppUser")] public string? AppUserId { get; set; }
         public AppUser? AppUser { get; set; }
+        public ClubCategory ClubCategory { get; set; }
     }
 }
