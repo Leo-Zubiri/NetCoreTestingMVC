a481f2ec82ff4d0b2ba7ff9f9257689cffda2140
Leo-Zubiri <leo.zubiri@outlook.com>
2024-01-02 22:37:39 -0600

    Add Model Club

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -3,7 +3,7 @@
     public class AppUser
     {
         public int? Pace { get; set; }
-        public int Mileage { get; set; }
-        public Address Address { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
     }
 }
